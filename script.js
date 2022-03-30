d7024c03a2d5118179b0d515bcb2c10f666baeaa
Camila Tuñas <camila_tunas@hotmail.com>
2022-03-29 22:44:22 -0300

    Cambios varios

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 URL = 'https://6243a0ce39aae3e3b744ef34.mockapi.io/jobpost'
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// git pasos habituales:
+// Ir a la carpeta que queremos agregar al repositorio. (click derecho abrir"git bash here")
+// 1. git init
+// 2. git add .
+// 3. git status (para chequear si hay archivos en rojo o verde)
+// 4. git commit -m  (para enviar el mensaje)
+// 5. git pull (sirve para traer los cambios)
+// 6.git push (sirve para mandar los cambios)
+// SI HACE FALTA VOLVER EN ALGÚN MOMENTO PARA VOLVER ATRÁS:
+// 5. git restore --staged nombredearchivo.extension
+
+
+// 4. git commit -m  (para enviar el mensaje)
+// 5. git pull (sirve para traer los cambios)
+// 6.git push (sirve para mandar los cambios)
